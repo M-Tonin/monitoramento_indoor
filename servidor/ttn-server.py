a476de3cd069d2d61b6d72bca3bd6d8736359ea7
THETNT\User <renatotbreis@gmail.com>
2020-06-15 08:22:58 -0300

    ttn-server.py
    -> Adicionado o server TTN

---
+++ servidor/ttn-server.py
@@ -0,0 +1,38 @@
+import time
+import ttn
+
+appId = 'monitoramento-indoor'
+accessKey = 'ttn-account-v2.L0jlR9qu-OzefvHzRzBq6SjaA6jPx8gBPfdo-6aLg4c'
+
+mysqlConfig = {
+  'user': 'renato',
+  'password': 'senha',
+  'host': '192.168.34.1',
+  'database': 'bla',
+  'raise_on_warnings': True
+}
+
+handler = ttn.HandlerClient (appId, accessKey)
+mqttClient = handler.data ()
+appClient = handler.application ()
+
+def uplinkCallback (msg, client):
+  print ('Uplink received from: ', msg.dev_id)
+  print (msg)
+
+def downlinkCallback (msg, client):
+  print ('Downlink sent to:', msg.dev_id)
+  print (msg)
+
+mqttClient.set_uplink_callback (uplinkCallback)
+mqttClient.set_downlink_callback (downlinkCallback)
+
+try:
+  mqttClient.connect ()
+  print ('Connected to TTN.')
+except:
+  print ('Failed to connect to TTN.')
+
+print ('Press any key to disconnect.')
+input ()
+mqttClient.close ()
