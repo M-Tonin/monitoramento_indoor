06e04bad874848f4451eda4a677da96fc2c1f593
THETNT\User <renatotbreis@gmail.com>
2020-06-15 14:44:40 -0300

    Uptade + Log de Erros
    -> Update no código ttn-server;
    -> Implementada uma mini interface para manter a conexão e melhorar o uso das funções do servidor;
    -> Adicionado o log de erros.

--- servidor/ttn-server.py
+++ servidor/ttn-server.py
@@ -1,20 +1,26 @@
+import codecs
 import time
 import ttn
 
 appId = 'monitoramento-indoor'
 accessKey = 'ttn-account-v2.L0jlR9qu-OzefvHzRzBq6SjaA6jPx8gBPfdo-6aLg4c'
+devId = '26011BC0'
 
 handler = ttn.HandlerClient (appId, accessKey)
 mqttClient = handler.data ()
 appClient = handler.application ()
 
+def b64toHex (msg):
+  msg = bytearray (msg, 'utf-8')
+  return codecs.encode (codecs.decode (msg, 'base64'), 'hex').decode ()
+
 def uplinkCallback (msg, client):
   print ('Uplink received from: ', msg.dev_id)
   print (msg)
+  print ('Payload (HEX): ' + b64toHex (msg.payload_raw))
 
-def downlinkCallback (msg, client):
-  print ('Downlink sent to:', msg.dev_id)
-  print (msg)
+def downlinkCallback (mid, client):
+  print ('Downlink sent: ' + str (mid))
 
 mqttClient.set_uplink_callback (uplinkCallback)
 mqttClient.set_downlink_callback (downlinkCallback)
@@ -24,7 +30,18 @@ try:
   print ('Connected to TTN.')
 except:
   print ('Failed to connect to TTN.')
+  input ()
+  raise Exception ('Closing program...')
 
-print ('Press any key to disconnect.')
-input ()
+print ('Enter QUIT to disconnect.')
+print ('Enter SEND to send Downlink.')
+
+cmd = 0
+while cmd != 'quit':
+  cmd = input ()
+
+  if cmd == 'send':
+    mqttClient.send (devId, '01', 1, False, 'replace')
+
+print ('Closing program...')
 mqttClient.close ()
